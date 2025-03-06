924c3e3489df5ac3cd5a599838efa980edbdb167
Eugene <160360231+Havpen@users.noreply.github.com>
2025-03-06 22:31:09 +0300

    added yml and dockerfile for you my badass

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS currency (
 CREATE TABLE IF NOT EXISTS account (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    currency_id INT NOT NULL,
+    currency_id INT,
     start_balance DECIMAL(15,2) NOT NULL DEFAULT 0,
     FOREIGN KEY (currency_id) REFERENCES currency(id) ON DELETE SET NULL
 );
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS transaction (
     account_id INT NOT NULL,
     amount DECIMAL(15,2) NOT NULL,
     timestamp DATE NOT NULL,
-    category_id INT NOT NULL,
+    category_id INT,  -- Сделаем nullable для возможности использования SET NULL
     FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
 );
@@ -31,8 +31,9 @@ CREATE TABLE IF NOT EXISTS transaction (
 CREATE TABLE IF NOT EXISTS external_transaction (
     id INT AUTO_INCREMENT PRIMARY KEY,
     transaction_id INT NOT NULL,
-    external_transaction_id INT NOT NULL,
+    external_transaction_id INT, 
     source_id VARCHAR(255) NOT NULL,
     FOREIGN KEY (transaction_id) REFERENCES transaction(id) ON DELETE CASCADE,
     FOREIGN KEY (external_transaction_id) REFERENCES external_transaction(id) ON DELETE SET NULL
 );
+
